65bfe646749a6c6a925b69077b858a9af4aa662c
Marco Pastorello <m.pastorello@websonica.net>
2012-06-25 17:34:39 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmw::Application.config.secret_token = '350c3c953c934991c4539e269f0b7ad55e13e487fadee280221ba83c73fc6dbcc5bb4aa50298b7602962739494a9dfc1365552e5e831a4930a3fe0c6d280587a'
